4305b554b7443e7f89bb91a345929b029cbac62a
mk-pmb <mk-pmb@users.noreply.github.com>
2014-03-27 18:42:47 +0100

    yamlconv --dive --keys --filter --omit

--- yamlconv.py
+++ yamlconv.py
@@ -6,14 +6,26 @@ from __future__ import division
 from sys import argv, stdin, stdout, stderr
 from codecs import open as cfopen
 import os.path
+
 import starbound
+
 import yaml
+# ^-- Performance geeks: For using C Loader and Dumper, refer to
+#     http://pyyaml.org/wiki/PyYAMLDocumentation
 
 
 
 def main(invocation, *cli_args):
     src_fn = dest_fn = '-'
     data = fmt = None
+
+    file_infos = (
+        'type=identifier',
+        'version',
+        'name',
+        'meta=get_metadata',
+        'content=data',
+        )
 
     cli_argi = iter(cli_args)
     for arg in cli_argi:
@@ -27,7 +39,7 @@ def main(invocation, *cli_args):
                 fmt = None
             continue
 
-        if arg in ('-O', '--dest'):
+        if arg in ('-o', '--dest'):
             dest_fn = cli_argi.next()
             continue
 
@@ -36,8 +48,24 @@ def main(invocation, *cli_args):
                 with stdio_or_cfopen(src_fn, 'r', 'utf8') as fh:
                     data = yaml.safe_load(fh)
                 continue
-            with starbound.open_file(src_fn, fmt) as file:
-                data = file.data
+            with starbound.open_file(src_fn, fmt) as fh:
+                data = obj2dict(fh, file_infos)
+            continue
+
+        if arg in ('-d', '--dive'):
+            data = dive_into_dict(data, cli_split(cli_argi.next()))
+            continue
+
+        if arg in ('-k', '--keys'):
+            data = data.keys()
+            continue
+
+        if arg in ('-F', '--filter'):
+            data = filter_dict(data, cli_split(cli_argi.next()))
+            continue
+
+        if arg in ('-o', '--omit'):
+            data = filter_dict(data, cli_split(cli_argi.next()), invert=True)
             continue
 
         if arg in ('-Y', '--dump'):
@@ -70,15 +98,75 @@ def guessfextfmt(force_fmt, fn):
     return os.path.splitext(fn)[1].lstrip('.')
 
 
+def dive_into_dict(basedict, path):
+    entry = basedict
+    cwd = []
+    for step in path:
+        try:
+            if isinstance(entry, (tuple, list,)):
+                step = int(step)
+            entry = entry[step]
+        except Exception as err:
+            raise KeyError('cannot dive deeper into dict', step, cwd, err)
+        cwd.append(step)
+    return entry
 
 
+def cli_split(arg):
+    arg = unicode(arg)
+    if arg == '':
+        return []
+    if arg[0].isalnum():
+        return [arg]
+    return arg[1:].split(arg[0])
 
 
+def obj2dict(obj, keys, dest = None):
+    if dest is None:
+        dest = {}
+    for key in keys:
+        alias = key.split('=', 1)
+        if len(alias) > 1:
+            alias, key = alias
+        else:
+            alias = key
+        val = None
+        try:
+            val = obj.__getattribute__(key)
+        except AttributeError as err:
+            pass
+        try:
+            if val is None:
+                val = obj.__getattr__(key)
+        except AttributeError as err:
+            pass
+        try:
+            val = val()
+        except Exception as err:
+            pass
+        dest[alias] = val
+    return dest
 
 
-
-
-
+def filter_dict(src, keys, invert=False, dest=None):
+    if dest is None:
+        dest = {}
+    if invert:
+        # copy all entries except those in keys
+        for key in src.viewkeys():
+            if key not in keys:
+                dest[key] = src[key]
+    else:
+        # copy the entries namedin keys
+        for key in keys:
+            alias = key.split('=', 1)
+            if len(alias) > 1:
+                alias, key = alias
+            else:
+                alias = key
+            if src.has_key(key):
+                dest[key] = src[key]
+    return dest
 
 
 
